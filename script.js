674b70318d5bc11aeedfe05bf0011a2d2378b2a2
Asif-newaz <asif.newaz0093@gmail.com>
2022-04-02 03:05:21 +0600

    comment some portion of code

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 "use strict";
 
-document.querySelector(".message").textContent =
-  "Correct number! Edited by Asif!";
-document.querySelector(".score").textContent = "10";
-document.querySelector(".highscore").textContent = "30";
-document.querySelector(".guess").value = "10";
+// document.querySelector(".message").textContent =
+//   "Correct number! Edited by Asif!";
+// document.querySelector(".score").textContent = "10";
+// document.querySelector(".highscore").textContent = "30";
+// document.querySelector(".guess").value = "10";
 
-console.log(document.querySelector(".message").textContent);
-console.log((document.querySelector(".guess").value = "10"));
+// console.log(document.querySelector(".message").textContent);
+// console.log((document.querySelector(".guess").value = "10"));
