b51a71bd08277231a3ee1e018d7751488c32dfd0
Asif-newaz <asif.newaz0093@gmail.com>
2022-04-09 00:46:41 +0600

    work with score update delete

--- script.js
+++ script.js
@@ -10,6 +10,7 @@
 // console.log((document.querySelector(".guess").value = "10"));
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
 
 document.querySelector(".number").textContent = secretNumber;
 
@@ -21,8 +22,22 @@ document.querySelector(".check").addEventListener("click", function () {
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "Correct number!";
   } else if (guess > secretNumber) {
-    document.querySelector(".message").textContent = "📈 Too high";
+    if (score > 1) {
+      document.querySelector(".message").textContent = "📈 Too high";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "💥 You lost the game!";
+      document.querySelector(".score").textContent = 0;
+    }
   } else if (guess < secretNumber) {
-    document.querySelector(".message").textContent= "📉 Too low"
+    if (score > 1) {
+      document.querySelector(".message").textContent = "📉 Too low";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "💥 You lost the game!";
+      document.querySelector(".score").textContent = 0;
+    }
   }
 });
