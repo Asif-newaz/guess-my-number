7d3ff89c765277f902a6181d172b7f233725026e
Asif-newaz <asif.newaz0093@gmail.com>
2022-04-20 19:43:25 +0600

    finish challenge 1

--- script.js
+++ script.js
@@ -9,9 +9,21 @@
 // console.log(document.querySelector(".message").textContent);
 // console.log((document.querySelector(".guess").value = "10"));
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector(".message").textContent = "Start guessing.....";
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = " ";
+
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
+});
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
